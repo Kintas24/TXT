3a47741c431219722ab4591bbe192a7fb1082208
Jorge Kintas <jorgekintas.jk@outlook.com>
2024-03-17 18:21:57 +0100

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+let elementoClicado=document.getElementById("elemento-clicado");
+let botaoTempo=document.getElementById("botao-clicado");
+let tempo=document.getElementById("tempo");
+
+botaoTempo.addEventListener("click",function(){
+
+});
