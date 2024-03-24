62b0f4db071d73e87668650801e48f369c37ee37
Jorge Kintas <jorgekintas.jk@outlook.com>
2024-03-24 12:38:50 +0100

    .

--- script.js
+++ script.js
@@ -7,5 +7,13 @@ titulo.innerText = "Angola é um país com clima tropical"
 prozeducacao.innerText = "um instituto de formação de programadores"
 frutas.innerHTML = "<li>Laranja</li> <li>limão</li>"+"<li>abacate</li>"
 frutas.innerHTML +="<li>Jaca</li>"
-listaOrdenada.innerHTML = "<li>https://www.youtube.com/"+"<li>https://www.linkedin.com"
-listaOrdenada.innerHTML +="<li>https://www.agt.minfin gov.ao"
+listaOrdenada.innerHTML = `
+<li><a href="https://www.linkedin.com/login/
+">LinkedIn</a></li>
+<li><a href="https://portaldocontribuinte.minfin.gov.ao/login
+">AGT Portal do Contribuínte</a></li>
+<li><a href="https://sepe.gov.ao/ao/">SEPE</a></li>
+<li><a href="https://www.saipem.com/en">SAIPEM</a></li>
+`;
+
+
